30087cf213fe4bf050204dc0833fe27ee3be5627
Ritesh Dalvi <riteshdalvi270@gmail.com>
2018-11-25 21:14:50 -0800

    Fixed the discovery.

--- SpringCloud-Coordination/03/java-microservices-spring-cloud-coordinating-services-m2/before/pluralsight-eureka-tollrate-service/src/main/java/pluralsight/demo/PluralsightEurekaTollrateServiceApplication.java
+++ SpringCloud-Coordination/03/java-microservices-spring-cloud-coordinating-services-m2/before/pluralsight-eureka-tollrate-service/src/main/java/pluralsight/demo/PluralsightEurekaTollrateServiceApplication.java
@@ -2,10 +2,10 @@ package pluralsight.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
-@EnableEurekaClient
 public class PluralsightEurekaTollrateServiceApplication {
 
 	public static void main(String[] args) {
