d4fafee6814949a519aac2c3c662f840454f2d6f
Ritesh Dalvi <rdalvi@prosper.com>
2018-11-25 19:57:54 -0800

    Moved the demo files and made some changes to eureka server and instance

---
+++ SpringCloud-Coordination/03/java-microservices-spring-cloud-coordinating-services-m2/before/pluralsight-eureka-tollrate-billboard/src/main/java/pluralsight/demo/DashboardController.java
@@ -0,0 +1,21 @@
+package pluralsight.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class DashboardController {
+	
+	@RequestMapping("/dashboard")
+	public String GetTollRate(@RequestParam int stationId, Model m) {
+		
+		RestTemplate rest = new RestTemplate();
+		TollRate tr = rest.getForObject("http://localhost:8085/tollrate/" + stationId, TollRate.class);
+		System.out.println("stationId: " + stationId);
+		m.addAttribute("rate", tr.getCurrentRate());
+		return "dashboard";
+	}
+}
---
+++ SpringCloud-Coordination/03/java-microservices-spring-cloud-coordinating-services-m2/before/pluralsight-eureka-tollrate-billboard/src/main/java/pluralsight/demo/PluralsightEurekaTollrateBillboardApplication.java
@@ -0,0 +1,12 @@
+package pluralsight.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluralsightEurekaTollrateBillboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluralsightEurekaTollrateBillboardApplication.class, args);
+	}
+}
